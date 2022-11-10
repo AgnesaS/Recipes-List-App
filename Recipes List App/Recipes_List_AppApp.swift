10a5bba4d7f5364a96c8599f1e900aeb20f6e8d4
AgnesaS <agnesaselimi8@gmail.com>
2022-11-10 12:09:43 +0100

    Initial Commit

---
+++ Recipes List App/Recipes_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipes_List_AppApp.swift
+//  Recipes List App
+//
+//  Created by Agnesa  on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Recipes_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
