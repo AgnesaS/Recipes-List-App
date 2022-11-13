083e34d7dd057d9c7e7475f8c8573e37c4e5b5ef
AgnesaS <agnesaselimi8@gmail.com>
2022-11-13 03:40:44 +0100

    Turned the Image to a Button in RecipesFeaturedView to display the recipes details when the card featured is tapped

--- Recipes List App/Views/RecipesFeaturedView.swift
+++ Recipes List App/Views/RecipesFeaturedView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RecipesFeaturedView: View {
     
     @EnvironmentObject var viewModel: RecipesViewModel
+    @State var isDetailViewShowing = false
     var body: some View {
         
         VStack(alignment: .leading, spacing: 0){
@@ -28,19 +29,28 @@ struct RecipesFeaturedView: View {
                         
                         if viewModel.recipes[index].featured == true {
                             
-                            ZStack{
-                                Rectangle()
-                                    .foregroundColor(.white)
-                                
-                                VStack{
-                                    Image(viewModel.recipes[index].image)
-                                        .resizable()
-                                        .clipped()
-                                        .aspectRatio(contentMode: .fill)
-                                    Text(viewModel.recipes[index].name)
-                                        .padding(5)
+                            Button(action: {
+                                self.isDetailViewShowing = true
+                            }, label: {
+                                ZStack{
+                                    Rectangle()
+                                        .foregroundColor(.white)
+                                    
+                                    VStack{
+                                        Image(viewModel.recipes[index].image)
+                                            .resizable()
+                                            .clipped()
+                                            .aspectRatio(contentMode: .fill)
+                                        Text(viewModel.recipes[index].name)
+                                            .padding(5)
+                                    }
                                 }
+                            })
+                            .sheet(isPresented: $isDetailViewShowing ){
+                                
+                                RecipesDetailsView(recipes: viewModel.recipes[index])
                             }
+                            .buttonStyle(PlainButtonStyle())
                             .frame(width: geo.size.width-40, height: geo.size.height-100, alignment: .center)
                             .cornerRadius(15)
                             .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.6), radius: 10, x: -5, y: 5)
